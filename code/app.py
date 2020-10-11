eb1e48811d5c03f4627f717c52b6cad2fd54cc07
p <ndojic@execom.eu>
2020-10-11 13:17:05 +0200

    adding login

--- code/app.py
+++ code/app.py
@@ -5,18 +5,21 @@ from flask_restful import Api
 
 from procedures.table_wrapper import db
 from resources.base_resource import generate_description, generate_resource
+from resources.login import Login
 from resources.tables import Tables
 
 # from resources.user import UserLogin, UserRegister, Users
 
 app = Flask(__name__)
 app.secret_key = "asdfqwer"
+app.config['JWT_SECRET_KEY'] = 'super-secret'  # Change this!
 api = Api(app)
 cors = CORS(app)  #TODO add fixed origin for production
 
 jwt = JWTManager(app)
 
 api.add_resource(Tables, "/tables")
+api.add_resource(Login, "/login")
 for table in db.tbls():
   try:
     url = f"/{table['table_name']}"
---
+++ code/resources/login.py
@@ -0,0 +1,31 @@
+import json
+
+from flask import request
+from flask_jwt_extended import (JWTManager, create_access_token,
+                                get_jwt_identity, jwt_required)
+from flask_restful import Resource
+from procedures.table_wrapper import TableWrapper
+
+user_service = TableWrapper("v_korisnik")
+
+
+class Login(Resource):
+  def post(self):
+    if not request.is_json:
+      return { "msg": "Missing JSON in request"}, 400
+
+    username = request.json.get('username', None)
+    password = request.json.get('password', None)
+    if not username:
+      return { "msg": "Missing username parameter"}, 400
+    if not password:
+      return { "msg": "Missing password parameter"}, 400
+
+    user = user_service.tbl_get(json.dumps({ "kr_username": username }))[0]
+    print(user, username, password)
+    if username != user["kr_username"] or password != user["kr_password"]:
+      return { "msg": "Bad username or password"}, 401
+
+    # Identity can be any data that is json serializable
+    access_token = create_access_token(identity=username)
+    return { 'access_token': access_token }, 200
